5fd5bb418d6a5428680f2bdee61fd42bcbefa03d
DEEPIKA-web20 <deepikasakthivel20@gmail.com>
2026-05-12 21:48:08 +0530

    Create CareerCard.jsx

---
+++ frontend/CareerCard.jsx
@@ -0,0 +1,15 @@
+function CareerCard({ title }) {
+  return (
+    <div style={{
+      background: "#0f172a",
+      color: "white",
+      padding: "10px",
+      margin: "10px",
+      borderRadius: "8px"
+    }}>
+      {title}
+    </div>
+  );
+}
+
+export default CareerCard;
